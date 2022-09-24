eb3f9e40d66301d1f39e78aa730255f447fd5002
Biagio <biagiopirainowork@gmail.com>
2022-09-24 14:19:26 +0100

    simple navbar implemented

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,16 +2,20 @@ import React, {Component} from 'react'
 import ToDo from './ToDo'
 import Targets from './Targets'
 import WeeklyTrack from './WeeklyTrack'
+import Navbar from './Navbar'
 import './MainPage.css'
 
 class MainPage extends Component {
    render() {
       return(
-         <div className="mainpage-container">
-            <ToDo />
-            <Targets />
-            <WeeklyTrack />
-         </div>
+         <React.Fragment>
+         <Navbar />
+            <div className="mainpage-container">
+               <ToDo />
+               <Targets />
+               <WeeklyTrack />
+            </div>
+         </React.Fragment>
       )
    }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+
+class Navbar extends Component {
+  render () {
+    return (
+      <nav className='navbar-main'>
+         <h1 className='navbar-title'>Keep it, Track it</h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
