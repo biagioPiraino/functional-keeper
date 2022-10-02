7e23830926371515ea99beaf2e82a0add9fe41c5
Biagio <biagiopirainowork@gmail.com>
2022-10-02 11:59:56 +0100

    retrieved weekly todos with moq data

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -11,12 +11,16 @@ class MainPage extends Component {
       super(props);
       this.state = {
          todolist: [],
-         dailyTargets: {}
+         dailyTargets: {},
+         weeklyTrack: []
       };
       this.fetchDailyTodo = this.fetchDailyTodo.bind(this);
+      this.fetchWeeklyTodo = this.fetchWeeklyTodo.bind(this);
+      this.getCurrentWeekNumber = this.getCurrentWeekNumber.bind(this);
    }
 
    componentDidMount() {
+      // Daily targets
       let dailyToDos = this.fetchDailyTodo();
       let targets, achieved, remaining;
 
@@ -24,13 +28,18 @@ class MainPage extends Component {
       achieved = dailyToDos.filter(x => x.done === true).length;
       remaining = targets - achieved;
 
+      // Weekly targets
+      let currentWeekNumber = this.getCurrentWeekNumber();
+      let weeklyTodo = this.fetchWeeklyTodo(currentWeekNumber);
+
       this.setState({
          todolist: dailyToDos,
          dailyTargets: {
             targets: targets,
             achieved: achieved,
             remaining: remaining
-         }
+         },
+         weeklyTrack: weeklyTodo
       });
    }
 
@@ -59,6 +68,31 @@ class MainPage extends Component {
       return toDoList;
    }
 
+   getCurrentWeekNumber() {
+      let currentDate, startDate, days;
+      currentDate = new Date();
+      startDate = new Date(currentDate.getFullYear(), 0, 1);
+      days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+      return Math.floor(days / 7);
+   }
+
+   fetchWeeklyTodo(weekNumber) {
+      const toDoList = ToDoData.filter(date => date.weekNumber === weekNumber);
+      let days = 0;
+      let weeklyTodo = [];
+
+      while (days < 7){
+         let toDos =
+            { planned: toDoList.filter(x => x.day === days).length, 
+              done: toDoList.filter(x => x.day === days && x.done).length,
+              undone: toDoList.filter(x => x.day === days && x.done === false).length}
+         weeklyTodo.push(toDos);
+         days++;
+      }
+      
+      console.log(weeklyTodo);
+   }
+
    render() {
       return(
          <React.Fragment>
@@ -66,7 +100,7 @@ class MainPage extends Component {
             <div className="mainpage-container">
                <ToDo items={this.state.todolist}/>
                <Targets items={this.state.dailyTargets}/>
-               <WeeklyTrack />
+               <WeeklyTrack items={this.state.weeklyTrack}/>
             </div>
          </React.Fragment>
       )
--- src/components/ToDoData.js
+++ src/components/ToDoData.js
@@ -2,6 +2,8 @@ export default [
    {
       id: '001',
       added: "27/9/2022",
+      day: 1,
+      weekNumber: 39,
       desc: 'Homework',
       category: 'Duty',
       done: true
@@ -9,6 +11,8 @@ export default [
    {
       id: '002',
       added: "27/9/2022",
+      day: 1,
+      weekNumber: 39,
       desc: 'Clean house',
       category: 'Duty',
       done: true
@@ -16,6 +20,8 @@ export default [
    {
       id: '003',
       added: "27/9/2022",
+      day: 1,
+      weekNumber: 39,
       desc: 'Watch movie',
       category: 'Leisure',
       done: false
